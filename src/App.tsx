38c8421872da13f5e907a231f06620cbb5537caf
Faizan Kiani <faizankiani947@gmail.com>
2026-06-23 11:58:49 -0700

    Added routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
+import Profile from "./pages/Profile";
+import onboarding from "./pages/Onboarding";
 
 function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/onboarding" element={<onboarding />} />
+
+
+
+        </Routes>
+    )
 }
 
 export default App
